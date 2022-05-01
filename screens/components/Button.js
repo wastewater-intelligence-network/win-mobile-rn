3165991777d2a7a85718b7928f30e84d3843c138
Abhishek Upperwal <a.upperwal@gmail.com>
2022-05-02 00:09:01 +0530

    linting

--- screens/components/Button.js
+++ screens/components/Button.js
@@ -1,51 +1,50 @@
 import React from 'react';
 import { Text, TouchableHighlight } from 'react-native';
 
-
 const Button = ({ onPress, children }) => {
-    const { buttonStyle, textStyle, pressedTextStyle } = styles;
-    const [ isPress, setIsPress ] = React.useState(false);
-    return(
-        <TouchableHighlight onPress={onPress} style = {buttonStyle} underlayColor={'#007aff'} 
-            onHideUnderlay={() => setIsPress(false)}
-            onShowUnderlay={() => setIsPress(true)}
-        >
-            <Text style={isPress ? pressedTextStyle: textStyle}>
-                {children}
-            </Text>
-        </TouchableHighlight>           
-    );
+	const { buttonStyle, textStyle, pressedTextStyle } = styles;
+	const [isPress, setIsPress] = React.useState(false);
+	return (
+		<TouchableHighlight
+			onPress={onPress}
+			style={buttonStyle}
+			underlayColor={'#007aff'}
+			onHideUnderlay={() => setIsPress(false)}
+			onShowUnderlay={() => setIsPress(true)}>
+			<Text style={isPress ? pressedTextStyle : textStyle}>
+				{children}
+			</Text>
+		</TouchableHighlight>
+	);
 };
 
 const styles = {
+	buttonStyle: {
+		flex: 1,
+		alsignSelf: 'stretch',
+		backgroundColor: '#fff',
+		boderRadius: 2,
+		borderWidth: 1,
+		borderColor: '#007aff',
+		marginLeft: 5,
+		marginRight: 5,
+		alignItems: 'center',
+	},
 
-    buttonStyle: {
-        flex: 1,
-        alsignSelf: 'stretch',
-        backgroundColor: '#fff',
-        boderRadius: 2,
-        borderWidth: 1,
-        borderColor: '#007aff',
-        marginLeft: 5,
-        marginRight: 5,
-        alignItems: 'center'
-    },
-
-    textStyle: {
-        color: '#007aff',
-        fontSize: 16,
-        paddingTop: 10,
-        paddingBottom: 10,
-        fontWeight: '400'
-    },
-    pressedTextStyle: {
-        color: '#fff',
-        fontSize: 16,
-        paddingTop: 10,
-        paddingBottom: 10,
-        fontWeight: '400'
-    }
-
+	textStyle: {
+		color: '#007aff',
+		fontSize: 16,
+		paddingTop: 10,
+		paddingBottom: 10,
+		fontWeight: '400',
+	},
+	pressedTextStyle: {
+		color: '#fff',
+		fontSize: 16,
+		paddingTop: 10,
+		paddingBottom: 10,
+		fontWeight: '400',
+	},
 };
 
 export default Button;
--- screens/constants.js
+++ screens/constants.js
@@ -1,79 +1,79 @@
 var Constants = {
-    debugDesc: {
-        text: "WIN-MOBILE-APP"
-    },
-    alertMessages: {
-        invalidSession: "Invalid session, Please try to login",
-        error: "Error!!",
-        invalidQRCode: "Invalid QR code scanned",
-        invalidQREntered: "Invalid QR code entered",
-        nodataFound: "No data found",
-        collectionAddedSuccessfully: "Collection point added successfully"
-    },
-    colors: {
-        primary: "#756BDE",
-        primaryDark: "#4e41d5",
-        gray: "#d4d4d4",
-        grayColor: 'gray',
-        black: 'black',
-        green: 'green',
+	debugDesc: {
+		text: 'WIN-MOBILE-APP',
+	},
+	alertMessages: {
+		invalidSession: 'Invalid session, Please try to login',
+		error: 'Error!!',
+		invalidQRCode: 'Invalid QR code scanned',
+		invalidQREntered: 'Invalid QR code entered',
+		nodataFound: 'No data found',
+		collectionAddedSuccessfully: 'Collection point added successfully',
+	},
+	colors: {
+		primary: '#756BDE',
+		primaryDark: '#4e41d5',
+		gray: '#d4d4d4',
+		grayColor: 'gray',
+		black: 'black',
+		green: 'green',
+	},
 
-    },
+	samplingType: {
+		stp: 'STP',
+		sps: 'SPS',
+		manhole: 'Manhole',
+	},
+	screenName: {
+		SampleCollector: 'SampleCollector',
+		SampleTransporter: 'SampleTransporter',
+		SampleAcceptance: 'SampleAcceptance',
+		SamplesList: 'SamplesList',
+		Login: 'Login',
+		Home: 'Home',
+		SiteSurvey: 'SiteSurvey',
+		Inventory: 'InventoryManagement',
+		Schedule: 'Schedules',
+		SiteSurveyList: 'SiteSurveyList',
+		collectionPoints: 'CollectionPoints',
+		ChooseLocale: 'ChooseLocale',
+	},
+	status: {
+		sampleCollected: 'sample_collected',
+		sampleInTransit: 'sample_in_transit',
+		sampleReceivedInLab: 'sample_received_in_lab',
+		sampleTestInProgress: 'sample_test_in_progress',
+		sampleResultOut: 'sample_result_out',
+	},
+	userRoles: {
+		collector: 'collector',
+		transporter: 'transporter',
+		admin: 'admin',
+		technician: 'technician',
+	},
+	homeMenuTitle: {
+		transporter: 'Sample\nTransportation',
+		technician: 'Accept\nSample',
+	},
+	endPoints: {
+		login: '/login',
+	},
+	storageKey: {
+		token: 'token',
+		timestamp: 'timestamp',
+	},
 
-    samplingType: {
-        stp: 'STP',
-        sps: 'SPS',
-        manhole: 'Manhole'
-    },
-    screenName: {
-        SampleCollector: 'SampleCollector',
-        SampleTransporter: 'SampleTransporter',
-        SampleAcceptance: 'SampleAcceptance',
-        SamplesList: 'SamplesList',
-        Login: 'Login',
-        Home: 'Home',
-        SiteSurvey: 'SiteSurvey',
-        Inventory:'InventoryManagement',
-        Schedule: 'Schedules',
-        SiteSurveyList: 'SiteSurveyList',
-        collectionPoints: 'CollectionPoints',
-    },
-    status: {
-        sampleCollected: 'sample_collected',
-        sampleInTransit: 'sample_in_transit',
-        sampleReceivedInLab: 'sample_received_in_lab',
-        sampleTestInProgress: 'sample_test_in_progress',
-        sampleResultOut: 'sample_result_out'
-    },
-    userRoles: {
-        collector: 'collector',
-        transporter: 'transporter',
-        admin: 'admin',
-        technician: 'technician'
-    },
-    homeMenuTitle: {
-        transporter: "Sample\nTransportation",
-        technician: "Accept\nSample"
-    },
-    endPoints: {
-        login: '/login'
-    },
-    storageKey: {
-        token: "token",
-        timestamp: "timestamp"
-    },
+	schemaName: {
+		UserDetails: 'UserDetails',
+	},
 
-    schemaName: {
-        UserDetails: "UserDetails"
-    },
-
-    scanCharater: {
-        length: 5,
-        regEx: /^(?:[A-Za-z0-9]+)$/
-    },
-    statusLog: {
-        error: 'error'
-    }
-}
+	scanCharater: {
+		length: 5,
+		regEx: /^(?:[A-Za-z0-9]+)$/,
+	},
+	statusLog: {
+		error: 'error',
+	},
+};
 
 export default Constants;
