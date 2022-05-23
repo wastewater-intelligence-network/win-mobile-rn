c2e51cb857e1c567601bcfb8da3ec74d29c29ad0
Abhishek Upperwal <a.upperwal@gmail.com>
2022-05-23 16:32:40 +0530

    linting

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { 
-	StyleSheet, 
-	Text, 
+import {
+	StyleSheet,
+	Text,
 	View,
 	TextInput,
 	Image,
 	TouchableHighlight,
 	ToastAndroid,
-	ActivityIndicator
+	ActivityIndicator,
 } from 'react-native';
 
 import WinLogoColor from '../../assets/win_logo_color.png';
@@ -16,16 +16,15 @@ import Constants from '../constants';
 import Util from '../Util';
 import DBManager from '../DBManager';
 
-export default function Login({navigation}) {
-	const [username, setUsername] = useState('')
-	const [password, setPassword] = useState('')
+export default function Login({ navigation }) {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 	const [loading, setLoading] = useState(false);
 
-	useEffect (() => {
+	useEffect(() => {
 		console.log('useeffect login called');
 		//isValidSession();
-	
-	},[]);
+	}, []);
 
 	const styles = StyleSheet.create({
 		container: {
@@ -40,110 +39,114 @@ export default function Login({navigation}) {
 			height: 200,
 		},
 		inputBox: {
-			width: "100%",
+			width: '100%',
 			padding: 10,
 			backgroundColor: '#E6E8ED',
 			margin: 10,
-			color: Constants.colors.grayColor
+			color: Constants.colors.grayColor,
 		},
 		button: {
 			width: '100%',
-			alignItems: "center",
-			backgroundColor: "#756BDE",
-			padding: 10
+			alignItems: 'center',
+			backgroundColor: '#756BDE',
+			padding: 10,
 		},
 		buttonText: {
 			fontSize: 17,
 			fontWeight: 'bold',
-			color: "#fff",
+			color: '#fff',
 		},
 		signinMessage: {
 			fontSize: 20,
-			fontWeight: "bold",
+			fontWeight: 'bold',
 			marginTop: 20,
 			marginBottom: 8,
-			fontFamily: "Quicksand",
-			color: "#756BDE"
-		}
-	});	
+			fontFamily: 'Quicksand',
+			color: '#756BDE',
+		},
+	});
 
 	const handleLogin = () => {
-		var auth = new Authentication()
-		//reset token and all 
+		var auth = new Authentication();
+		//reset token and all
 		setLoading(true);
-		auth.login(username, password)
-			.then((res) => {
-				if(res) {
-					console.log(`${Constants.debugDesc.text} response=${res.roles}`);
-					var array = res.roles
-					ToastAndroid.showWithGravity("Login Successful", ToastAndroid.SHORT, ToastAndroid.BOTTOM)
-                   // navigation.navigate('Home', res.roles)
-					navigation.replace('Home', res.roles)
-					setLoading(false);
-				} else {
-					ToastAndroid.show("Could not login. Please check your username and password", ToastAndroid.LONG)
-					setLoading(false);
-				}
-			})
-	}
+		auth.login(username, password).then(res => {
+			if (res) {
+				console.log(
+					`${Constants.debugDesc.text} response=${res.roles}`,
+				);
+				var array = res.roles;
+				ToastAndroid.showWithGravity(
+					'Login Successful',
+					ToastAndroid.SHORT,
+					ToastAndroid.BOTTOM,
+				);
+				// navigation.navigate('Home', res.roles)
+				navigation.replace('Home', {
+					roles: res.roles,
+				});
+				setLoading(false);
+			} else {
+				ToastAndroid.show(
+					'Could not login. Please check your username and password',
+					ToastAndroid.LONG,
+				);
+				setLoading(false);
+			}
+		});
+	};
 
 	const isValidSession = () => {
 		let isValid = DBManager.isValidSession(navigation);
 		console.log(` is session valid=${isValid}`);
-	}
+	};
 	isValidSession();
 
 	return (
-		
-		<View style={styles.container} >
-			<Image
-				source={WinLogoColor}
-				style={styles.winLogo}
-			/>
+		<View style={styles.container}>
+			<Image source={WinLogoColor} style={styles.winLogo} />
 			<Text
 				style={{
-					textAlign: "center",
-					fontFamily: "Quicksand",
+					textAlign: 'center',
+					fontFamily: 'Quicksand',
 					marginTop: 10,
 					marginBottom: 10,
-					color: Constants.colors.grayColor
-				}}
-			>An initiative to manage the spread and impact of SARS-CoV-2 through Wastewater-based Epidemiology (WBE)</Text>
-			
+					color: Constants.colors.grayColor,
+				}}>
+				An initiative to manage the spread and impact of SARS-CoV-2
+				through Wastewater-based Epidemiology (WBE)
+			</Text>
+
 			<Text style={styles.signinMessage}>SIGN IN TO YOUR ACCOUNT</Text>
 			<TextInput
 				style={styles.inputBox}
 				width="100%"
-				placeholder='Username'
+				placeholder="Username"
 				onChangeText={setUsername}
 				selectionColor="#756BDE"
-                autoCapitalize='none'
+				autoCapitalize="none"
 				placeholderTextColor={Constants.colors.grayColor}
-
 			/>
 			<TextInput
 				style={styles.inputBox}
 				width="100%"
-				placeholder='Password'
+				placeholder="Password"
 				onChangeText={setPassword}
 				selectionColor="#756BDE"
-                autoCapitalize='none'
+				autoCapitalize="none"
 				secureTextEntry={true}
 				placeholderTextColor={Constants.colors.grayColor}
-				/>
-			{ loading === false ?
-			<TouchableHighlight 
-				style={styles.button}
-				underlayColor={Constants.colors.primaryDark}
-				onPress={handleLogin}
-			>
-				<Text style={styles.buttonText}>LOGIN</Text>
-			</TouchableHighlight>
-			:
-			<ActivityIndicator size='small'/>
-
-			}
+			/>
+			{loading === false ? (
+				<TouchableHighlight
+					style={styles.button}
+					underlayColor={Constants.colors.primaryDark}
+					onPress={handleLogin}>
+					<Text style={styles.buttonText}>LOGIN</Text>
+				</TouchableHighlight>
+			) : (
+				<ActivityIndicator size="small" />
+			)}
 		</View>
-			
 	);
 }
--- screens/SiteSurvey/SiteSurvey.js
+++ screens/SiteSurvey/SiteSurvey.js
@@ -1,223 +1,248 @@
 import React, { useState, useEffect } from 'react';
 import {
-    View, 
-    Text,
-    StyleSheet,
-    StatusBar,
-    TextInput,
-    PermissionsAndroid,
-    ToastAndroid,
-    ActivityIndicator
-    } from 'react-native';
+	View,
+	Text,
+	StyleSheet,
+	StatusBar,
+	TextInput,
+	PermissionsAndroid,
+	ToastAndroid,
+	ActivityIndicator,
+} from 'react-native';
 import Button from '../components/Button';
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
 import Geolocation from '@react-native-community/geolocation';
 import SampleTracking from '../../controllers/sample_tracking';
 import Spinner from '../Spinner';
 
-const SiteSurvey = ({navigation}) => {
+const SiteSurvey = ({ navigation }) => {
+	const [location, setLocation] = useState('');
+	const [locationData, setLocationData] = useState(undefined);
 
-    const [location, setLocation] = useState('');
-    const [locationData, setLocationData] = useState(undefined);
+	const [samplingSite, setsamplingSite] = useState('');
+	const [siteID, setSiteID] = useState('');
+	const [selectedSamplingSite, setSelectedSamplingSite] = useState('');
+	const [preferredTypes, setPreferredTypes] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	let placeholderTextColor = '#d3d3d1';
 
-    const [samplingSite, setsamplingSite] = useState('');
-    const [siteID, setSiteID] = useState('');
-    const [selectedSamplingSite, setSelectedSamplingSite] = useState('');
-    const [preferredTypes, setPreferredTypes] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-    let placeholderTextColor = "#d3d3d1";
-    
-    const resetField = () => {
-        setsamplingSite('');
-        setSiteID('');
-        setSelectedSamplingSite('');
-        setPreferredTypes('');
-    };
+	const resetField = () => {
+		setsamplingSite('');
+		setSiteID('');
+		setSelectedSamplingSite('');
+		setPreferredTypes('');
+	};
 
-    const submitHandle = () => {
+	const submitHandle = () => {
+		var sampleTracking = new SampleTracking();
+		setIsLoading(true);
+		console.log(
+			`location=${locationData.coords.latitude} ${locationData.coords.longitude} sampling site=${samplingSite} site id=${siteID} Site type=${selectedSamplingSite} preferred type=${preferredTypes}`,
+		);
+		sampleTracking
+			.siteSurveyCollected(
+				locationData,
+				samplingSite,
+				siteID,
+				selectedSamplingSite,
+				preferredTypes,
+				navigation,
+			)
+			.then(res => {
+				console.log('getting response', res);
+				setIsLoading(false);
+				resetField();
+				ToastAndroid.showWithGravity(
+					'Submitted Successfully',
+					ToastAndroid.SHORT,
+					ToastAndroid.BOTTOM,
+				);
+			});
+	};
 
-        var sampleTracking = new SampleTracking();
-        setIsLoading(true);
-        console.log(`location=${locationData.coords.latitude} ${locationData.coords.longitude} sampling site=${samplingSite} site id=${siteID} Site type=${selectedSamplingSite} preferred type=${preferredTypes}`);
-        sampleTracking.siteSurveyCollected(locationData, samplingSite, siteID, selectedSamplingSite,preferredTypes, navigation)
-            .then((res) => {
-                console.log('getting response', res);
-                setIsLoading(false);
-                resetField();
-                ToastAndroid.showWithGravity("Submitted Successfully", ToastAndroid.SHORT, ToastAndroid.BOTTOM)
-            })
-    };
+	const requestLocationPermission = async () => {
+		try {
+			const granted = await PermissionsAndroid.request(
+				PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+				{
+					title: 'WinMobile App Location Permission',
+					message: 'WinMobile App needs access to your location ',
+					buttonPositive: 'OK',
+				},
+			);
+			if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+				getLocation();
+			} else {
+				requestLocationPermission();
+			}
+		} catch (err) {
+			console.warn(err);
+		}
+	};
 
-    const requestLocationPermission = async () => {
-        try {
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            {
-              title: "WinMobile App Location Permission",
-              message: "WinMobile App needs access to your location ",
-              buttonPositive: "OK"
-            }
-          );
-          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            getLocation();
-          } else {
-            requestLocationPermission();
-          }
-        } catch (err) {
-          console.warn(err);
-        }
-      };
+	const getLocation = () => {
+		Geolocation.getCurrentPosition(data => {
+			setLocationData(data);
+			let lat_long =
+				'lat ' +
+				data.coords.latitude +
+				',long ' +
+				data.coords.longitude;
+			setLocation(lat_long);
+		});
+	};
 
-      const getLocation = () => {
-            Geolocation.getCurrentPosition((data) =>{
-                setLocationData(data)
-                let lat_long = 'lat ' +data.coords.latitude + ',long ' + data.coords.longitude
-                setLocation(lat_long);               
-            })
-         };
+	useEffect(() => {
+		requestLocationPermission();
+	}, []);
 
-    useEffect(() => {
-        requestLocationPermission();
-          
+	return (
+		<View style={styles.container}>
+			<Text style={styles.pageHeading}>Site Survey</Text>
 
+			<View>
+				<Text style={styles.titleLabel}>Location</Text>
+				<View style={styles.inputView}>
+					<TextInput
+						style={styles.TextInput}
+						placeholder="Location"
+						value={location}
+						onChangeText={location =>
+							setLocation(location.substr(0, 15))
+						}
+						editable={false}
+					/>
+				</View>
+			</View>
 
-      }, []);
+			<View>
+				<Text style={styles.titleLabel}>Sampling Site Name</Text>
+				<View style={styles.inputView}>
+					<TextInput
+						style={styles.TextInput}
+						placeholder="Sampling site"
+						value={samplingSite}
+						onChangeText={site =>
+							setsamplingSite(site.substr(0, 15))
+						}
+					/>
+				</View>
+			</View>
 
-    return(
-        <View style={styles.container}>
-            <Text style={styles.pageHeading}>Site Survey</Text>		
+			<View>
+				<Text style={styles.titleLabel}>Sampling Site (ID)</Text>
+				<View style={styles.inputView}>
+					<TextInput
+						style={styles.TextInput}
+						placeholder="Site ID"
+						value={siteID}
+						onChangeText={sID => setSiteID(sID.substr(0, 15))}
+					/>
+				</View>
+			</View>
 
-                <View>
-                        <Text style={styles.titleLabel}>Location</Text>
-                        <View style={styles.inputView}>
-                            <TextInput
-                                style={styles.TextInput}
-                                placeholder="Location"                               
-                                value={location}
-                                onChangeText={(location) => setLocation(location.substr(0, 15)) }
-                                editable={false}
-                            />
-                        </View>
-                </View>	
+			<View>
+				<Text style={styles.titleLabel}>Sampling Site Type</Text>
+				<View style={styles.inputView}>
+					<Picker
+						style={styles.pickerStyle}
+						selectedValue={selectedSamplingSite}
+						onValueChange={(itemValue, itemIndex) =>
+							setSelectedSamplingSite(itemValue)
+						}>
+						<Picker.Item label="Unknown" value="" />
+						<Picker.Item label="STP" value="STP" />
+						<Picker.Item label="SPS" value="SPS" />
+						<Picker.Item label="Manhole" value="Manhole" />
+					</Picker>
+				</View>
+			</View>
 
-                <View>
-                        <Text style={styles.titleLabel}>Sampling Site Name</Text>
-                        <View style={styles.inputView}>
-                            <TextInput
-                                style={styles.TextInput}
-                                placeholder="Sampling site"
-                                value={samplingSite}
-                                onChangeText={(site) => setsamplingSite(site.substr(0, 15)) }
-                            />
-                        </View>
-                </View>	
+			<View>
+				<Text style={styles.titleLabel}>Sampling Method</Text>
+				<View style={styles.inputView}>
+					<Picker
+						style={styles.pickerStyle}
+						selectedValue={preferredTypes}
+						onValueChange={(itemValue, itemIndex) =>
+							setPreferredTypes(itemValue)
+						}>
+						<Picker.Item label="Unknown" value="" />
+						<Picker.Item
+							label="Dip Sampling"
+							value="Dip Sampling"
+						/>
+						<Picker.Item
+							label="Ladle Sampling"
+							value="Ladle Sampling"
+						/>
+						<Picker.Item
+							label="Mechanised Sampling"
+							value="Mechanised Sampling"
+						/>
+					</Picker>
+				</View>
+			</View>
 
-                <View>
-                        <Text style={styles.titleLabel}>Sampling Site(ID)</Text>
-                        <View style={styles.inputView}>
-                            <TextInput
-                                style={styles.TextInput}
-                                placeholder="Site ID"
-                                value={siteID}
-                                onChangeText={(sID) => setSiteID(sID.substr(0, 15)) }
-                            />
-                        </View>
-                </View>	
-
-
-                <View>
-                        <Text style={styles.titleLabel}>Sampling Site Types</Text>
-                        <View style={styles.inputView}>
-                            <Picker
-                                style={styles.pickerStyle}
-                                selectedValue={selectedSamplingSite}
-                                onValueChange={(itemValue, itemIndex) =>
-                                    setSelectedSamplingSite(itemValue)
-                                }>
-                                <Picker.Item label="Select Sampling site Types" value="" />
-                                <Picker.Item label="STP" value="STP" />
-                                <Picker.Item label="SPS" value="SPS" />
-                                <Picker.Item label="Manhole" value="Manhole" />
-                            </Picker>
-                        </View>
-                </View>
-
-                <View>
-                        <Text style={styles.titleLabel}>Prefered Types</Text>
-                        <View style={styles.inputView}>
-                            <Picker
-                                style={styles.pickerStyle}
-                                selectedValue={preferredTypes}
-                                onValueChange={(itemValue, itemIndex) =>
-                                    setPreferredTypes(itemValue)
-                                }>
-                                <Picker.Item label="Select Prefered Types" value="" />
-                                <Picker.Item label="dip sampling" value="dip sampling" />
-                                <Picker.Item label="laddle sampling" value="laddle sampling" />
-                                <Picker.Item label="mechanised sampling" value="mechanised sampling" />
-                            </Picker>
-                        </View>
-                </View>
-
-                {
-                    isLoading == false ? 
-                    <View style={{marginLeft: 20, marginRight:20, marginTop:50, height: 40}}>
-                        <Button onPress = {() => submitHandle()}>
-                            Submit
-                        </Button>
-                     </View>
-                     :
-                     <Spinner /> 
-                }
-                
-        </View>
-    );
-}
-
+			{isLoading == false ? (
+				<View
+					style={{
+						marginLeft: 20,
+						marginRight: 20,
+						marginTop: 50,
+						height: 45,
+					}}>
+					<Button onPress={() => submitHandle()}>Submit</Button>
+				</View>
+			) : (
+				<Spinner />
+			)}
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
 		marginTop: StatusBar.currentHeight,
-        flex: 1
+		flex: 1,
 	},
 
-    pageHeading: {
+	pageHeading: {
 		fontSize: 25,
-		fontWeight: "bold",
+		fontWeight: 'bold',
 		marginTop: 0,
 		marginBottom: 8,
-		fontFamily: "Quicksand",
-		color: "#756BDE",
-        alignSelf: 'center'
+		fontFamily: 'Quicksand',
+		color: '#756BDE',
+		alignSelf: 'center',
 	},
-    TextInput: {
-        height: 45,
-        flex: 1,
-        padding: 2,
-        //marginLeft: 20,
-        width: "95%",
-    },
+	TextInput: {
+		height: 45,
+		flex: 1,
+		padding: 2,
+		//marginLeft: 20,
+		width: '95%',
+	},
 
-    pickerStyle: {
-        width: '100%',
-        marginTop: -5 
-    },
-    inputView: {
-        height: 45,
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 5,
-        borderColor: "#d3d3d3",
-        borderWidth: 1,
-        alignItems: "center"
-      },
+	pickerStyle: {
+		width: '100%',
+		marginTop: -5,
+	},
+	inputView: {
+		height: 45,
+		marginLeft: 30,
+		marginRight: 30,
+		marginTop: 5,
+		borderColor: '#d3d3d3',
+		borderWidth: 1,
+		alignItems: 'center',
+	},
 
-      titleLabel: {
-        marginLeft: 30,
-        marginTop: 10,
-        color: '#003f5c'
-    },
-
- });
+	titleLabel: {
+		marginLeft: 30,
+		marginTop: 10,
+		color: '#003f5c',
+	},
+});
 
 export default SiteSurvey;
